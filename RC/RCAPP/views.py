bce40204408092ca42fa42098a0cd1104d7479a6
Sabuka-Dumbo <anonym.sas@proton.me>
2024-12-23 17:25:24 +0000

    Dec 23, 2024, 9:25 PM

---
+++ RC/RCAPP/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+
+# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def register(request):
+    return render(request, "register.html")
