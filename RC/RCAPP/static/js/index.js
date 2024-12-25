289a41fb66e1ea535d29025725985a7775ff7540
Sabuka-Dumbo <anonym.sas@proton.me>
2024-12-25 10:54:51 +0000

    Dec 25, 2024, 2:54 PM

--- RC/RCAPP/static/js/index.js
+++ RC/RCAPP/static/js/index.js
@@ -4,22 +4,51 @@ const burger_span2 = document.getElementById("burger-span2");
 const burger_span3 = document.getElementById("burger-span3");
 const burger_nav = document.getElementById("burger-nav");
 
+let is_open = false;
+let is_cooldown = false;
+
 burger_menu.addEventListener("click", function() {
-    burger_nav.style.animation = "burger_nav ease 1s";
-    burger_span1.style.animation = "span1_open ease 1s";
-    burger_span2.style.animation = "span2_open ease 1s";
-    burger_span3.style.animation = "span3_open ease 1s";
+    if (is_cooldown == false) {
+        is_cooldown = true;
+        if (is_open == false) {
+            burger_nav.style.animation = "burger_nav_open ease 1s";
+            burger_span1.style.animation = "span1_open ease 1s";
+            burger_span2.style.animation = "span2_open ease 1s";
+            burger_span3.style.animation = "span3_open ease 1s";
 
-    burger_span1.addEventListener("animationend", function() {
-        burger_nav.style.animation = '';
-        burger_span1.style.animation = '';
-        burger_span2.style.animation = '';
-        burger_span3.style.animation = '';
+            burger_span1.addEventListener("animationend", function() {
+                burger_nav.style.animation = '';
+                burger_span1.style.animation = '';
+                burger_span2.style.animation = '';
+                burger_span3.style.animation = '';
 
-        burger_span1.style.top = "10px";
-        burger_span1.style.rotate = "45deg";
-        burger_span2.style.opacity = "0";
-        burger_span3.style.top = "-10px";
-        burger_span3.style.rotate = "-45deg";
-    })
+                burger_span1.style.top = "10px";
+                burger_span1.style.rotate = "45deg";
+                burger_span2.style.opacity = "0";
+                burger_span3.style.top = "-10px";
+                burger_span3.style.rotate = "-45deg";
+
+                is_open = true;
+                is_cooldown = false;
+            })
+        } else {
+            burger_nav.style.animation = "burger_nav_close ease 1s";
+            burger_span1.style.animation = "span1_open ease 1s";
+            burger_span2.style.animation = "span2_open ease 1s";
+            burger_span3.style.animation = "span3_open ease 1s";
+
+            burger_span1.addEventListener("animationend", function() {
+                burger_nav.style.animation = '';
+                burger_span1.style.animation = '';
+                burger_span2.style.animation = '';
+                burger_span3.style.animation = '';
+
+                burger_span1.style.top = "10px";
+                burger_span1.style.rotate = "45deg";
+                burger_span2.style.opacity = "0";
+                burger_span3.style.top = "-10px";
+                burger_span3.style.rotate = "-45deg";
+            })
+        }
+    }
 })
