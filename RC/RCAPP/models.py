747c78484862eae492e2863b92634b110e0c3f41
Sabuka-Dumbo <anonym.sas@proton.me>
2024-12-28 20:02:52 +0000

    Dec 29, 2024, 12:02 AM

--- RC/RCAPP/models.py
+++ RC/RCAPP/models.py
@@ -4,3 +4,36 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     pass
+
+class Categories(models.Model):
+    category = models.CharField(max_length=150)
+
+    def __str__(self):
+        return "${self.category} category"
+
+class Status(models.Model):
+    status = models.CharField(max_length=150)
+
+    def __str__(self):
+        return "${self.status} status"
+
+class Visibility(models.Model):
+    visibility = models.CharField(max_length=150)
+
+    def __str__(self):
+        return "${self.visibility} visibility"
+
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=150)
+    project_description = models.CharField(max_length=1000)
+    project_category = models.ManyToManyField(Categories, related_name="project_categories")
+    project_status = models.ForeignKey(Status, on_delete=models.CASCADE, related_name="project_status")
+    project_visibility = models.ForeignKey(Visibility, on_delete=models.CASCADE, related_name="project_visibility")
+    create_date = models.DateField()
+    last_update = models.DateField()
+    project_owner = models.ManyToManyField(User, related_name="project_owner")
+    project_members = models.ManyToManyField(User, related_name="project_members")
+    invite_out_request = models.ManyToManyField(User, related_name="people_who_we_invite")
+    invite_in_request = models.ManyToManyField(User, related_name="people_who_wants_to_join")
+    
