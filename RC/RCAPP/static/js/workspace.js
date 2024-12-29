e98b4bdd2273d9f1eee6ba619fd2b1779836ccf1
Sabuka-Dumbo <anonym.sas@proton.me>
2024-12-29 10:35:15 +0000

    Dec 29, 2024, 2:35 PM

--- RC/RCAPP/static/js/workspace.js
+++ RC/RCAPP/static/js/workspace.js
@@ -1,4 +1,4 @@
-/*const navbar = document.getElementById("navbar");
+const navbar = document.getElementById("navbar");
 const burgermenu = document.getElementById("burgermenu");
 const span1 = document.getElementById("span1");
 const span2 = document.getElementById("span2");
@@ -108,7 +108,6 @@ notify_icon.addEventListener("click", function() {
 })
 
 
-*/
 
 // Initialize the GoJS diagram
 let diagram = null;
