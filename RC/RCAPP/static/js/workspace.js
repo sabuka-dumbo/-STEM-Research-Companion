928404e715cfa1315bdb4387e6fc18ac4286dd66
Sabuka-Dumbo <anonym.sas@proton.me>
2024-12-31 15:16:33 +0000

    Dec 31, 2024, 7:16 PM

--- RC/RCAPP/static/js/workspace.js
+++ RC/RCAPP/static/js/workspace.js
@@ -8,31 +8,21 @@ const navbar2 = document.getElementById("navbar-main-part");
 const mm_div = document.getElementById("mm-div");
 const mm_link1 = document.getElementById("mm-link1")
 const mm_link2 = document.getElementById("mm-link2");
-const navbar_navs_mm = document.getElementById("navbar-navs-mm");
 
 function open_mm() {
-  navbar_navs_mm.innerHTML = `
-   <h1 class="navbar-line" id="mm-link2">|</h1>
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-diagram-3-fill navbar-icon" viewBox="0 0 16 16">
-      <path fill-rule="evenodd" d="M6 3.5A1.5 1.5 0 0 1 7.5 2h1A1.5 1.5 0 0 1 10 3.5v1A1.5 1.5 0 0 1 8.5 6v1H14a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0v-1A.5.5 0 0 1 2 7h5.5V6A1.5 1.5 0 0 1 6 4.5zm-6 8A1.5 1.5 0 0 1 1.5 10h1A1.5 1.5 0 0 1 4 11.5v1A1.5 1.5 0 0 1 2.5 14h-1A1.5 1.5 0 0 1 0 12.5zm6 0A1.5 1.5 0 0 1 7.5 10h1a1.5 1.5 0 0 1 1.5 1.5v1A1.5 1.5 0 0 1 8.5 14h-1A1.5 1.5 0 0 1 6 12.5zm6 0a1.5 1.5 0 0 1 1.5-1.5h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5z"/>
-    </svg>
-    <h1 class="navbar-nav normal-font">Mind maps</h1>
-  `;
-
   mm_div.style.display = "block";
-  mm_link1.style.color = "#DA8359"
-  mm_link2.style.color = "#DA8359"
+  mm_link1.style.color = "#DA8359";
+  mm_link2.style.color = "#DA8359";
 }
 
 const charts_div = document.getElementById("charts-div");
-const charts_link1 = document.getElementById("charts-link1")
+const charts_link1 = document.getElementById("charts-link1");
 const charts_link2 = document.getElementById("charts-link2");
-const navbar_navs_charts = document.getElementById("navbar-navs-charts");
 
 function open_charts() {
   charts_div.style.display = "block";
-  charts_link1.style.color = "#DA8359"
-  charts_link2.style.color = "#DA8359"
+  charts_link1.style.color = "#DA8359";
+  charts_link2.style.color = "#DA8359";
 }
 
 let current_name = "";
