56b42114c867e13c77d7bc45f4a9bcb13abfb799
Sabuka-Dumbo <anonym.sas@proton.me>
2024-12-31 15:10:21 +0000

    Dec 31, 2024, 7:10 PM

--- RC/RCAPP/static/js/workspace.js
+++ RC/RCAPP/static/js/workspace.js
@@ -5,27 +5,20 @@ const span2 = document.getElementById("span2");
 const span3 = document.getElementById("span3");
 const navbar2 = document.getElementById("navbar-main-part");
 
+const nav_links = document.getElementById("nav_links");
+const working_divs = document.getElementById("working_div");
+
 const mm_div = document.getElementById("mm-div");
 const mm_link1 = document.getElementById("mm-link1")
 const mm_link2 = document.getElementById("mm-link2");
 const navbar_navs_mm = document.getElementById("navbar-navs-mm");
 
 function open_mm() {
-  close_all_divs();
-  
-  setTimeout(() => {
-    navbar_navs_mm.innerHTML = `
-      <h1 class="navbar-line" id="mm-link2">|</h1>
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-diagram-3-fill navbar-icon" viewBox="0 0 16 16">
-        <path fill-rule="evenodd" d="M6 3.5A1.5 1.5 0 0 1 7.5 2h1A1.5 1.5 0 0 1 10 3.5v1A1.5 1.5 0 0 1 8.5 6v1H14a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0v-1A.5.5 0 0 1 2 7h5.5V6A1.5 1.5 0 0 1 6 4.5zm-6 8A1.5 1.5 0 0 1 1.5 10h1A1.5 1.5 0 0 1 4 11.5v1A1.5 1.5 0 0 1 2.5 14h-1A1.5.5 0 0 1 0 12.5zm6 0A1.5 1.5 0 0 1 7.5 10h1a1.5 1.5 0 0 1 1.5 1.5v1A1.5 1.5 0 0 1 8.5 14h-1A1.5 1.5 0 0 1 6 12.5zm6 0a1.5 1.5 0 0 1 1.5-1.5h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5z"/>
-      </svg>
-      <h1 class="navbar-nav normal-font">Mind maps</h1>
-    `;
-
-    mm_div.style.display = "block";
-    mm_link1.style.color = "#DA8359";
-    mm_link2.style.color = "#DA8359";
-  }, 1);
+  working_divs.style.display = '';
+  mm_div.style.display = "block";
+  nav_links.style.color = '';
+  mm_link1.style.color = "#DA8359";
+  mm_link2.style.color = "#DA8359";
 }
 
 const charts_div = document.getElementById("charts-div");
@@ -34,45 +27,11 @@ const charts_link2 = document.getElementById("charts-link2");
 const navbar_navs_charts = document.getElementById("navbar-navs-charts");
 
 function open_charts() {
-close_all_divs();
-
-  navbar_navs_charts.innerHTML = `
-   <h1 class="navbar-line" id="charts-link2">|</h1>
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pie-chart-fill navbar-icon" viewBox="0 0 16 16">
-      <path d="M15.985 8.5H8.207l-5.5 5.5a8 8 0 0 0 13.277-5.5zM2 13.292A8 8 0 0 1 7.5.015v7.778zM8.5.015V7.5h7.485A8 8 0 0 0 8.5.015"/>
-    </svg>
-    <h1 class="navbar-nav normal-font">Charts</h1>
-  `;
-
+  working_divs.style.display = '';
   charts_div.style.display = "block";
+  nav_links.style.color = '';
   charts_link1.style.color = "#DA8359";
   charts_link2.style.color = "#DA8359";
-}
-
-function close_all_divs() {
-  navbar_navs_mm.innerHTML = `
-  <h1 class="navbar-line" id="mm-link2">|</h1>
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-diagram-3 navbar-icon" viewBox="0 0 16 16">
-      <path fill-rule="evenodd" d="M6 3.5A1.5 1.5 0 0 1 7.5 2h1A1.5 1.5 0 0 1 10 3.5v1A1.5 1.5 0 0 1 8.5 6v1H14a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0v-1A.5.5 0 0 1 2 7h5.5V6A1.5 1.5 0 0 1 6 4.5zM8.5 5a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5zM0 11.5A1.5 1.5 0 0 1 1.5 10h1A1.5 1.5 0 0 1 4 11.5v1A1.5 1.5 0 0 1 2.5 14h-1A1.5 1.5 0 0 1 0 12.5zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm4.5.5A1.5 1.5 0 0 1 7.5 10h1a1.5 1.5 0 0 1 1.5 1.5v1A1.5 1.5 0 0 1 8.5 14h-1A1.5 1.5 0 0 1 6 12.5zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm4.5.5a1.5 1.5 0 0 1 1.5-1.5h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5z"/>
-  </svg>
-  <h1 class="navbar-nav normal-font">Mind maps</h1>
- `;
-
- mm_div.style.display = "none";
- mm_link1.style.color = '';
- mm_link2.style.color = '';
-
-  navbar_navs_charts.innerHTML = `
-    <h1 class="navbar-line" id="charts-link2">|</h1>
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pie-chart navbar-icon" viewBox="0 0 16 16">
-        <path d="M7.5 1.018a7 7 0 0 0-4.79 11.566L7.5 7.793zm1 0V7.5h6.482A7 7 0 0 0 8.5 1.018M14.982 8.5H8.207l-4.79 4.79A7 7 0 0 0 14.982 8.5M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8"/>
-    </svg>
-    <h1 class="navbar-nav normal-font">Charts</h1>
-  `;
-
-  charts_div.style.display = "none";
-  charts_link1.style.color = '';
-  charts_link2.style.color = '';
 }
 
 let current_name = "";
